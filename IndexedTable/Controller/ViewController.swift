e3f2662129088434e80b0eddbe7b10c300c5c35e
devxris <chuwei0915@gmail.com>
2017-09-23 23:01:29 +0800

    Initial Commit with MVC

---
+++ IndexedTable/Controller/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  IndexedTable
+//
+//  Created by Chris Huang on 23/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
